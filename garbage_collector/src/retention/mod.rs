c3da9bf8b522d74e213310e18e2a935fd0327894
Praveen Kumar <pkumar@influxdata.com>
2024-11-11 15:12:28 +0000

    chore: remove unused crates
    - garbage_collector, iox_data_generator, wal, wal_inspect,
      object_store_mem_cache are all removed

--- garbage_collector/src/retention/mod.rs
+++
@@ -1,3 +0,0 @@
-/// Logic for flagging parquet files for deletion based on retention settings
-pub(crate) mod parquet;
-pub(crate) mod partition;
--- wal/src/blocking.rs
+++
@@ -1,7 +0,0 @@
-//! Synchronous reader & writer implementations of the IOx WAL.
-
-mod reader;
-pub(crate) use reader::{ClosedSegmentReader, Error as ReaderError};
-
-mod writer;
-pub(crate) use writer::{Error as WriterError, OpenSegmentFileWriter};
