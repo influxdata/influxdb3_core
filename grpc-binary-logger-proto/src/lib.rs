e67ebfb6e17b66a646f8c9fd64cd36965b642ce7
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2025-05-15 15:20:44 -0400

    fix: Remove crates that have been removed from IOx
    And aren't used anywhere in core.

--- grpc-binary-logger-proto/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![allow(clippy::derive_partial_eq_without_eq, clippy::use_self)]
-
-// Workaround for "unused crate" lint false positives.
-use workspace_hack as _;
-
-pub mod proto {
-    tonic::include_proto!("grpc.binarylog.v1");
-}
-pub use proto::*;
