30e69c51dafdf23f075ea6a6c214f6da43648c9a
Trevor Hilton <thilton@influxdata.com>
2025-05-20 16:37:52 -0400

    chore: prune dml and import_export

--- import_export/src/file/mod.rs
+++
@@ -1,6 +0,0 @@
-/// Code to import/export files
-mod export;
-mod import;
-
-pub use export::{ExportError, Output, RemoteExporter};
-pub use import::{Error, ExportedContents, RemoteImporter};
--- import_export/src/lib.rs
+++
@@ -1,7 +0,0 @@
-//! Import/export utilities for IOx
-
-// Workaround for "unused crate" lint false positives.
-use workspace_hack as _;
-
-/// Import/Export data to files
-pub mod file;
